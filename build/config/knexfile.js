f23085ef786dabae53aa4d95704a3071101cb015
unknown <joaoif.eletro@gmail.com>
2024-11-30 17:28:04 -0300

    Att

--- build/config/knexfile.js
+++ build/config/knexfile.js
@@ -3,17 +3,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const knex_1 = __importDefault(require("knex"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const config = {
-    development: {
-        client: 'mysql2',
-        connection: {
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME
-        }
-    }
-};
-exports.default = config;
+const db = (0, knex_1.default)({
+    client: 'mysql2',
+    connection: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
+    },
+});
+exports.default = db;
--- build/models/equipamentosModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.equipamentos = void 0;
-const equipamentos = [
-    { id: 1, nome: 'Colete balístico', categoria: 'Defesa', atributoPrim: 'Resistência', valorAtri: 50 },
-    { id: 2, nome: 'Luvas reforçadas', categoria: 'Defesa', atributoPrim: 'Agilidade', valorAtri: 20 },
-    // Adicione mais equipamentos conforme necessário
-];
-exports.equipamentos = equipamentos;
